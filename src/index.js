141cba722a07ffdd618c2c88054b30a713035b81
Matthew <turbodieseldodge@gmail.com>
2023-01-18 10:14:24 -0600

    ramen wrecker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 const url = `http://localhost:3000/ramens`
 const ramenMenu = document.querySelector('#ramen-menu')
-const ramenImage = document.querySelector('#ramen-detail .detail-image')
-const ramenName = document.querySelector('#ramen-detail .name')
-const ramenRestaurant = document.querySelector('#ramen-detail .restaurant')
-const ramenRating = document.querySelector('#rating-display')
-const ramenComment = document.querySelector('#comment-display')
 const newRamenForm = document.querySelector('#new-ramen')
 
 let globalRamen = -1
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(currentRaman => {
-            addToMenu(currentRaman)
-        })
-        showDetails(data[0])
-        globalRamen = data[0].id
+.then(response => response.json())
+.then(data => {
+    data.forEach(currentRaman => {
+        addToMenu(currentRaman)
     })
+    showDetails(data[0])
+    globalRamen = data[0].id
+})
 
 const addToMenu = (ramen) => {
     let img = document.createElement('img')
@@ -27,14 +22,23 @@ const addToMenu = (ramen) => {
         showDetails(ramen)
         globalRamen = ramen.id
     })
-   ramenMenu.append(img)
+    ramenMenu.append(img)
 }
 
 const showDetails = (ramen) => {
+    const ramenRating = document.querySelector('#rating-display')
     ramenRating.innerText = ramen.ramenRating
+
+    const ramenRestaurant = document.querySelector('#ramen-detail .restaurant')
     ramenRestaurant.innerText = ramen.ramenRestaurant
+
+    const ramenName = document.querySelector('#ramen-detail .name')
     ramenName.innerText = ramen.name
+
+    const ramenImage = document.querySelector('#ramen-detail .detail-image')
     ramenImage.src = ramen.image
+
+    const ramenComment = document.querySelector('#comment-display')
     ramenComment.innerText = ramen.comment
 }
 
