3644dbfbb7feaf2ba833471779b66d51844c4ec4
Matthew <turbodieseldodge@gmail.com>
2023-01-18 16:02:06 -0600

    totorial

--- src/index.js
+++ src/index.js
@@ -1,58 +1,48 @@
-const url = `http://localhost:3000/ramens`
+function getRamen() {
+    fetch (`http://localhost:3000/ramens`)
+    .then(response => response.json())
+    .then(ramens => renderRamens(ramens))
+}
+
 const ramenMenu = document.querySelector('#ramen-menu')
-const newRamenForm = document.querySelector('#new-ramen')
 
-let globalRamen = -1
+const ramenImage = document.querySelector('#ramen-detail > img')
+const ramenName = document.querySelector('#ramen-detail > h2')
+const ramenRes = document.querySelector('#ramen-detail > h3')
+const ramenRating = document.querySelector('#rating-display')
+const ramenComment = document.querySelector('#comment-display')
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-    data.forEach(currentRaman => {
-        addToMenu(currentRaman)
+function renderRamens(ramens) {
+   ramens.forEach(ramen => {
+    const menuImage = document.createElement('img')
+    menuImage.src = ramen.image
+    ramenMenu.append(menuImage)
+    menuImage.addEventListener('click', () =>{
+        ramenName.textContent = ramen.name
+        ramenImage.src = ramen.image
+        ramenRes.textContent = ramen.restaurant
+        ramenRating.textContent = ramen.rating
+        ramenComment.textContent = ramen.comment
     })
-    showDetails(data[0])
-    globalRamen = data[0].id
 })
+}
 
-const addToMenu = (ramen) => {
-    let img = document.createElement('img')
-    img.src = ramen.image
-    img.id = `id${ramen.id}`
-    img.addEventListener('click', () => {
-        showDetails(ramen)
-        globalRamen = ramen.id
+function postRam(){
+    const ramenForm = document.querySelector('#new-ramen')
+    ramenForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let ramenObj = {
+            name: event.target.name.value,
+            restaurant: event.target.restaurant.value,
+            image: event.target.image.value,
+            rating: parseInt(event.target.rating.value),
+            comment: event.target['new-comment'].value
+        }
+        renderRamens([ramenObj])
     })
-    ramenMenu.append(img)
 }
 
-const showDetails = (ramen) => {
-    const ramenRating = document.querySelector('#rating-display')
-    ramenRating.innerText = ramen.ramenRating
-
-    const ramenRestaurant = document.querySelector('#ramen-detail .restaurant')
-    ramenRestaurant.innerText = ramen.ramenRestaurant
-
-    const ramenName = document.querySelector('#ramen-detail .name')
-    ramenName.innerText = ramen.name
-
-    const ramenImage = document.querySelector('#ramen-detail .detail-image')
-    ramenImage.src = ramen.image
-
-    const ramenComment = document.querySelector('#comment-display')
-    ramenComment.innerText = ramen.comment
-}
-
-const addNewRamen = (e) => {
-    e.preventDefault()
-    let body = {
-        name: e.target.name.value,
-        restaurant: e.target.restaurant.value,
-        rating: e.target.rating.value,
-        comment: e.target['new-comment'].value,
-        image: e.target.image.value
-    }
-    addToMenu(body)
-}
+postRam()
 
 
 
